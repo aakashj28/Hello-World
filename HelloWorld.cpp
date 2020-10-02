5c6b0d632c7dbe1f7e03a67d563fe17e66c6ad26
aakashj28 <55941524+aakashj28@users.noreply.github.com>
2020-10-03 03:24:26 +0530

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
+#include <iostream> //header files
 using namespace std;
 
-int main() {
-    cout << "Hello World" << endl;
+int main() { //main driver function
+    cout << "Hello World" << "\n"; //outputs to standard console
     return 0;
-}
+} //end program
